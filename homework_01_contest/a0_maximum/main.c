978f9c1890a2f6c81c963a1c45366c115ea75b11
Dmitri S. Guskov <dsguskov@gmail.com>
2024-11-27 14:32:29 +0300

    Update main.c
    Replaced tabs with 2 spaces

--- homework_01_contest/a0_maximum/main.c
+++ homework_01_contest/a0_maximum/main.c
@@ -11,52 +11,52 @@
 
 int get_next_int(int *result)
 {
-	int out_getchar = 0, is_negative = 0, temp = 0, is_ready = 0;
-	char ch;
+  int out_getchar = 0, is_negative = 0, temp = 0, is_ready = 0;
+  char ch;
 
-	while ((out_getchar = getchar())) {
-		ch = (char)out_getchar;
-		if ((out_getchar == EOF) || (ch == ' ') || (ch == '\n')) {
-		    if (is_ready)
-		        *result = temp * (1 - 2 * is_negative);
-            break;
-		}
-		else if (ch == '-') {
-            if (is_negative)
-                return 0;
-            is_negative = 1;
-		}
-		else if ((ch >= '0') && (ch <= '9')) {
-            temp = temp * 10 + (ch - '0');
-            is_ready = 1;
-		}
-		else {
-            return 0;
-		}
-	}
-	return 1;
+  while ((out_getchar = getchar())) {
+    ch = (char)out_getchar;
+    if ((out_getchar == EOF) || (ch == ' ') || (ch == '\n')) {
+      if (is_ready)
+        *result = temp * (1 - 2 * is_negative);
+      break;
+    }
+    else if (ch == '-') {
+      if (is_negative)
+        return 0;
+      is_negative = 1;
+    }
+    else if ((ch >= '0') && (ch <= '9')) {
+      temp = temp * 10 + (ch - '0');
+      is_ready = 1;
+    }
+    else {
+      return 0;
+    }
+  }
+  return 1;
 } // get_next_int
 
 int main(void)
 {
-    int count = 0, temp = 0, max = INT_MIN, freq = 0;
-    
-    if (!get_next_int(&count)) // Считываем количество
-        return EXIT_FAILURE;
+  int count = 0, temp = 0, max = INT_MIN, freq = 0;
+  
+  if (!get_next_int(&count)) // Считываем количество
+    return EXIT_FAILURE;
 
-    if (count < 1)             // Проверяем количество
-        return EXIT_FAILURE;
-    
-    while (count-- && get_next_int(&temp)) {
-        if (temp > max) {     // Новый максимум 
-            max = temp;
-            freq = 1;
-        }
-        else if (temp == max) // Повтор максимума
-            freq++;
+  if (count < 1)       // Проверяем количество
+    return EXIT_FAILURE;
+  
+  while (count-- && get_next_int(&temp)) {
+    if (temp > max) {   // Новый максимум 
+      max = temp;
+      freq = 1;
     }
+    else if (temp == max) // Повтор максимума
+      freq++;
+  }
 
-    printf("%d\n", freq);
+  printf("%d\n", freq);
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 } // main
