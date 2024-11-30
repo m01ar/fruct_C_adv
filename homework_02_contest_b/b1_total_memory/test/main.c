b6c8027ef751c4f3295e7b4d28c89b610785bf81
Dmitri S. Guskov <dsguskov@gmail.com>
2024-11-30 19:22:37 +0300

    Create main.c

---
+++ homework_02_contest_b/b1_total_memory/test/main.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <stdint.h>
+
+typedef struct node {
+	uint64_t address;
+	size_t size;
+	char comment[64];
+	struct node *next;
+} node_t;
+
+node_t *compose_list()
+{
+	int node_cnt = 0;
+	node_t *head_node = NULL, *last_node = NULL, *this_node = NULL;
+
+	if (scanf("%d", &node_cnt) < 1)
+		return NULL;
+
+	for (int i = 0; i < node_cnt; i++) {
+		this_node = malloc(sizeof(node_t));
+		if (scanf("%"SCNu64" %zu",
+		          &(this_node->address), &(this_node->size)) < 2) {
+			free(this_node);
+			return NULL;
+		}
+
+		if (!head_node)
+			head_node = this_node;
+
+		if (!last_node)
+			last_node = this_node;
+		else {
+			last_node->next = this_node;
+			last_node = this_node;
+		}
+	}
+	return head_node;
+}
+
+void print_list(node_t *node)
+{
+	if (!node)
+		return;
+	printf("%"PRIu64" %zu\n", node->address, node->size);
+	print_list(node->next);
+}
+
+void free_list(node_t *node)
+{
+	if (!node)
+		return;
+	free_list(node->next);
+//	puts("freeing node...");
+	free(node);
+}
+
+uint64_t findMaxBlock(node_t *head)
+{
+	node_t max_size_node = {0, 0, "", NULL};
+	for (node_t *node = head; node; node = node->next)
+	{
+		if (node->size > max_size_node.size) {
+			max_size_node.size = node->size;
+			max_size_node.address = node->address;
+		}
+	}
+	return max_size_node.address;
+}
+
+size_t totalMemoryUsage(node_t *head)
+{
+  size_t result = 0;
+  for (node_t *node = head; node; node = node->next)
+    result += node->size;
+
+  return result;
+}
+
+int main()
+{
+	node_t *head_node = NULL;
+
+	head_node = compose_list();
+	puts("");
+	// for (this_node = head_node; this_node; this_node = this_node->next)
+	//     printf("%"PRIu64" %zu\n", this_node->address, this_node->size);
+  // 	print_list(head_node);
+	printf("%"PRIu64"\n", totalMemoryUsage(head_node));
+	free_list(head_node);
+	return EXIT_SUCCESS;
+}
