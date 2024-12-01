fcdf2fe49495ecf80471e8a89283cd0e95c0d2f5
Dmitri S. Guskov <dsguskov@gmail.com>
2024-12-01 12:40:51 +0300

    Update function.c
    Replace tabs with spaces

--- homework_02_contest_b/b0_max_block/function.c
+++ homework_02_contest_b/b0_max_block/function.c
@@ -1,12 +1,11 @@
 uint64_t findMaxBlock(list *head)
 {
-	list max_size_node = {0, 0, "", NULL};
-	for (list *node = head; node; node = node->next)
-	{
-		if (node->size > max_size_node.size) {
-			max_size_node.size = node->size;
-			max_size_node.address = node->address;
-		}
-	}
-	return max_size_node.address;
+  list max_size_node = {0, 0, "", NULL};
+  for (list *node = head; node; node = node->next) {
+    if (node->size > max_size_node.size) {
+      max_size_node.size = node->size;
+      max_size_node.address = node->address;
+    }
+  }
+  return max_size_node.address;
 }
